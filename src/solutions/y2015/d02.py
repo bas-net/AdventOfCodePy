4f17e35185a7c7d2b29f82ddfe608b9613533d30
bas-net <88321527+bas-net@users.noreply.github.com>
2021-10-17 01:39:53 +0200

    2015.02.1

---
+++ src/solutions/y2015/d02.py
@@ -0,0 +1,19 @@
+def p1(input: str) -> str:
+    total = 0
+    for package in input.split():
+        dimensions = list(map(int, package.split('x')))
+
+        sides = [dimensions[0] * dimensions[1],
+                 dimensions[1] * dimensions[2],
+                 dimensions[2] * dimensions[0]]
+        sides.sort()
+
+        package_total = sides[0] * 3 + sides[1] * 2 + sides[2] * 2
+
+        total += package_total
+
+    return total
+
+
+def p2(input: str) -> str:
+    pass
