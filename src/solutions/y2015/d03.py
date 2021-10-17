e7eaed386274638638c214634cdfa3f964fc8cac
bas-net <88321527+bas-net@users.noreply.github.com>
2021-10-17 12:35:59 +0200

    2015.03.2

--- src/solutions/y2015/d03.py
+++ src/solutions/y2015/d03.py
@@ -1,8 +1,9 @@
 import solutions.y2015.lib2015
 from collections import defaultdict
 
+
 def p1(input: str) -> str:
-    visited = defaultdict(lambda : 0)
+    visited = defaultdict(lambda: 0)
     x = 0
     y = 0
     visited[(x, y)] += 1
@@ -23,4 +24,27 @@ def p1(input: str) -> str:
 
 
 def p2(input: str) -> str:
-    pass
+    visited = defaultdict(lambda: 0)
+    xs = [0, 0]
+    ys = [0, 0]
+
+    visited[(xs[0], ys[0])] += 1
+    visited[(xs[1], ys[1])] += 1
+
+    index = 0
+
+    for c in input:
+        if c == '^':
+            ys[index % 2] += 1
+        elif c == 'v':
+            ys[index % 2] -= 1
+        elif c == '<':
+            xs[index % 2] -= 1
+        elif c == '>':
+            xs[index % 2] += 1
+        else:
+            raise 'Error'
+        visited[(xs[index % 2], ys[index % 2])] += 1
+        index += 1
+
+    return len(visited)
