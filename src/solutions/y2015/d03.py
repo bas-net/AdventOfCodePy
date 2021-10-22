7785dcd20f36c4c3a2b238a9389a4b6d90e16ec0
bas-net <88321527+bas-net@users.noreply.github.com>
2021-10-23 01:03:39 +0200

    2015.03 refactoring

--- src/solutions/y2015/d03.py
+++ src/solutions/y2015/d03.py
@@ -1,39 +1,44 @@
 from collections import defaultdict
+from typing import Dict, List
+from dataclasses import dataclass
 
 
 def p1(input_string: str) -> str:
-    visited = defaultdict(lambda: 0)
-
-    x = 0
-    y = 0
-    visited[(x, y)] += 1
-    for c in input_string:
-        (x, y) = determine_new_coordinates(x, y, c)
-        visited[(x, y)] += 1
-
-    return len(visited)
+    return do_stuff(input_string, 1)
 
 
 def p2(input_string: str) -> str:
-    visited = defaultdict(lambda: 0)
-    
-    x = [0, 0]
-    y = [0, 0]
-
-    visited[(x[0], y[0])] += 1
-    visited[(x[1], y[1])] += 1
-
-    index = 0
-
-    for c in input_string:
-        (x[index % 2], y[index % 2]) = determine_new_coordinates(x[index % 2], y[index % 2], c)
-        visited[(x[index % 2], y[index % 2])] += 1
-        index += 1
-
-    return len(visited)
+    return do_stuff(input_string, 2)
 
 
-def determine_new_coordinates(x, y, character):
+def do_stuff(instructions: str, number_of_walkers: int) -> int:
+    visited_count = init_visited_count_store()
+
+    coords = init_point_list(number_of_walkers)
+
+    set_all_points_to_visited(visited_count, coords)
+
+    for index, instr in enumerate(instructions):
+        coord_under_edit = index % number_of_walkers
+
+        coords[coord_under_edit] = determine_instruction_result(
+            coords[coord_under_edit],
+            instr)
+        
+        visited_count[coords[coord_under_edit]] += 1
+
+    return len(visited_count)
+
+
+@dataclass(unsafe_hash=True, frozen=True)
+class Point:
+    x: int = 0
+    y: int = 0
+
+
+def determine_instruction_result(coords: Point, character) -> Point:
+    (x, y) = (coords.x, coords.y)
+
     if character == '^':
         y += 1
     elif character == 'v':
@@ -44,6 +49,18 @@ def determine_new_coordinates(x, y, character):
         x += 1
     else:
         raise Exception('Invalid character')
-    return (x, y)
+
+    return Point(x, y)
 
 
+def init_visited_count_store() -> Dict[Point, int]:
+    return defaultdict(lambda: 0)
+
+
+def init_point_list(number: int) -> List[Point]:
+    return [Point() for _ in range(number)]
+
+
+def set_all_points_to_visited(visited_count: Dict[Point, int], coords: List[Point]):
+    for coord in coords:
+        visited_count[coord] += 1
