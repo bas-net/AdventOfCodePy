6633a5fda886072d50ab55405d32b8467320e1aa
bas-net <88321527+bas-net@users.noreply.github.com>
2021-10-26 20:06:20 +0200

    Some todo's

--- src/solutions/y2015/d01.py
+++ src/solutions/y2015/d01.py
@@ -9,6 +9,7 @@ def p2(input_string: str) -> str:
     pointer = 0
     index = 1
 
+    # TODO Possibly some kind of "run till sum/aggregation = or is less than more than/lambda expr"
     for i in convert_parentheses_to_ints(input_string):
         pointer += i
         if pointer < 0:
@@ -18,5 +19,6 @@ def p2(input_string: str) -> str:
     raise Exception('Invalid state')
 
 
+# TODO Possibly turn to a generic "character to int mapping"
 def convert_parentheses_to_ints(input_string: str) -> List[int]:
     return [1 if x == '(' else -1 if x == ')' else 0 for x in input_string]
--- src/solutions/y2015/d02.py
+++ src/solutions/y2015/d02.py
@@ -3,8 +3,10 @@ from typing import List
 import solutions.y2015.lib2015
 
 
+# TODO Make some kind of class with sides and dimensions to represent a cube.
+
 def p1(input_str: str) -> str:
-    def func(package: str) -> int:
+    def get_packaging_for_package(package: str) -> int:
         dimensions = get_dimensions(package)
 
         sides = [dimensions[0] * dimensions[1],
@@ -16,11 +18,11 @@ def p1(input_str: str) -> str:
         # Every side * 2, except the smallest, which has the slack, so * 3.
         return sides[0] * 3 + sides[1] * 2 + sides[2] * 2
 
-    return solutions.y2015.lib2015.process_by_line_aggregate(input_str, func, sum)
+    return solutions.y2015.lib2015.process_by_line_aggregate(input_str, get_packaging_for_package, sum)
 
 
 def p2(input_str: str) -> str:
-    def func(package: str) -> int:
+    def get_ribbon_for_package(package: str) -> int:
         dimensions = get_dimensions(package)
 
         dimensions.sort()
@@ -32,9 +34,10 @@ def p2(input_str: str) -> str:
 
         return perimeter + ribbon
 
-    return solutions.y2015.lib2015.process_by_line_aggregate(input_str, func, sum)
+    return solutions.y2015.lib2015.process_by_line_aggregate(input_str, get_ribbon_for_package, sum)
 
 
+# TODO Possibly split out into get width height length/xyz
 def get_dimensions(package: str) -> List[int]:
     return list(map(int, package.split('x')))
 
