31aca81654f7e4d7f88444d288bac2a5fbb83a13
bas-net <88321527+bas-net@users.noreply.github.com>
2021-10-30 16:08:48 +0200

    Graph to lib

--- src/solutions/y2015/d13.py
+++ src/solutions/y2015/d13.py
@@ -3,125 +3,7 @@ import re
 from dataclasses import dataclass, field
 from collections import defaultdict
 
-import solutions.y2015.lib2015
-
-
-@dataclass(frozen=True)
-class Node:
-    name: str
-    vertices_to: Dict[str, int] = field(default_factory=dict)
-
-
-@dataclass(frozen=True)
-class Vertex:
-    from_node: Node
-    to_node: Node
-    weight: int
-
-
-@dataclass
-class PotentialPath:
-    distance_total: int = 0
-    visited_nodes: List[str] = field(default_factory=list)
-
-
-class Graph:
-    nodes: Dict[str, Node]
-    vertices: List[Vertex]
-
-    def __init__(self) -> None:
-        self.nodes = {}
-        self.vertices = []
-
-    def add_vertex(self, node_name_soure: str, node_name_to: str, weight: int):
-        source_node = self.get_node_from_name(node_name_soure)
-        to_node = self.get_node_from_name(node_name_to)
-
-        source_node.vertices_to[node_name_to] = weight
-        to_node.vertices_to[node_name_soure] = weight
-
-        self.vertices.append(Vertex(source_node, to_node, weight))
-
-    def get_node_from_name(self, node_name):
-        if node_name in self.nodes:
-            return self.nodes[node_name]
-        else:
-            node_source = Node(node_name)
-            self.nodes[node_name] = node_source
-            # print(f'Createing node {node_name}')
-            return node_source
-
-    def tsp_longest_path_round(self) -> int:
-        """Traveling salesman problem"""
-        # print('Calculating TSP')
-        longest = None
-
-        def get_potential_paths(node_name, visited):
-            paths = []
-            for to in self.nodes[node_name].vertices_to:
-                if to not in visited:
-                    this_path = [x for x in visited]
-                    this_path.append(to)
-                    for x in get_potential_paths(to, this_path):
-                        paths.append(x)
-            if len(paths) > 0:
-                return paths
-            else:
-                return [visited]
-
-        paths = []
-        for start_node in self.nodes:
-            for x in [x for x in get_potential_paths(start_node, [start_node])]:
-                x.append(start_node)
-                paths.append(x)
-
-        for path in paths:
-            # print(path)
-            try:
-                length = sum(self.nodes[path[i]].vertices_to[path[i + 1]]
-                             for i in range(len(path) - 1))
-                if longest is None or longest < length:
-                    longest = length
-                    # print(path)
-            except KeyError:
-                pass
-
-        return longest
-
-    def tsp_longest_path(self) -> int:
-        """Traveling salesman problem"""
-        # print('Calculating TSP')
-        longest = None
-
-        def get_potential_paths(node_name, visited):
-            paths = []
-            for to in self.nodes[node_name].vertices_to:
-                if to not in visited:
-                    this_path = [x for x in visited]
-                    this_path.append(to)
-                    for x in get_potential_paths(to, this_path):
-                        paths.append(x)
-            if len(paths) > 0:
-                return paths
-            else:
-                return [visited]
-
-        paths = []
-        for start_node in self.nodes:
-            for x in [x for x in get_potential_paths(start_node, [start_node])]:
-                paths.append(x)
-
-        for path in paths:
-            try:
-                length = sum(self.nodes[path[i]].vertices_to[path[i + 1]]
-                             for i in range(len(path) - 1))
-                if longest is None or longest < length:
-                    longest = length
-                    # print(path)
-            except KeyError:
-                pass
-
-        return longest
+from solutions.y2015.lib2015 import Graph, process_by_line_aggregate
 
 
 @dataclass
@@ -147,7 +29,7 @@ def p1(input_string: str) -> str:
 
         return Line(matches.group('name'), matches.group('namenextto'), matches.group('action'), int(matches.group('change')))
 
-    lines = solutions.y2015.lib2015.process_by_line_aggregate(
+    lines = process_by_line_aggregate(
         input_string, extract, list)
 
     x = defaultdict(lambda: 0)
@@ -172,7 +54,7 @@ def p2(input_string: str) -> str:
 
         return Line(matches.group('name'), matches.group('namenextto'), matches.group('action'), int(matches.group('change')))
 
-    lines = solutions.y2015.lib2015.process_by_line_aggregate(
+    lines = process_by_line_aggregate(
         input_string, extract, list)
 
     x = defaultdict(lambda: 0)
