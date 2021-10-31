7c81249785b3f19a4ad6a4dc68ec0938ba1cd3d6
bas-net <88321527+bas-net@users.noreply.github.com>
2021-10-31 15:54:27 +0100

    Day 7 deduplication

--- src/solutions/y2015/d07.py
+++ src/solutions/y2015/d07.py
@@ -4,91 +4,20 @@ import solutions.y2015.lib2015
 
 
 def p1(input_string: str) -> str:
-    # letter : value
-    wires = {}
-    # output : (s1, s2, op)
-    gates = {}
+    (wires, gates) = setup_wires_and_gates(input_string)
 
-    def func(instruction: str) -> None:
-        wire_instr = re.findall(r'^(\d+)\s->\s([a-z]+)$', instruction)
-        if wire_instr:
-            wires[wire_instr[0][1]] = int(wire_instr[0][0])
-            return
-        gate_instr = re.search(
-            r'^(?P<s1>[a-z0-9]+)?\s?(?P<gate>AND|LSHIFT|RSHIFT|NOT|OR)\s(?P<s2>[a-z0-9]+)\s->\s(?P<dest>[a-z]+)$', instruction)
-        if gate_instr:
-            gate = gate_instr.group('gate')
-            dest = gate_instr.group('dest')
-            source_2 = gate_instr.group('s2')
-            if gate == 'NOT':
-                gates[dest] = (source_2, None, 'NOT')
-                return
-
-            source_1 = gate_instr.group('s1')
-            gates[dest] = (source_1, source_2, gate)
-            return
-
-        direct_instr = re.search(
-            r'^(?P<s1>[a-z]+)\s->\s(?P<dest>[a-z]+)$', instruction)
-
-        if direct_instr is not None:
-            source_1 = direct_instr.group('s1')
-            dest = direct_instr.group('dest')
-            gates[dest] = (source_1, None, 'DIRECT')
-            return
-
-        raise Exception()
-
-    solutions.y2015.lib2015.process_by_line(input_string, func)
-
-    queue = list(gates)
-
-    while len(queue) > 0:
-        dest = queue.pop(0)
-        handling = gates[dest]
-        has_sources = True
-        if handling[0] not in wires and not handling[0].isnumeric():
-            has_sources = False
-        if handling[2] not in ['NOT', 'DIRECT']:
-            if handling[1] not in wires and not handling[1].isnumeric():
-                has_sources = False
-
-        if not has_sources:
-            queue.append(dest)
-            continue
-
-        source_1 = int(handling[0]) if handling[0].isnumeric(
-        ) else wires[handling[0]]
-        if handling[2] == 'NOT':
-            wires[dest] = (~source_1) & 0xFFFF
-            continue
-        if handling[2] == 'DIRECT':
-            wires[dest] = source_1
-            continue
-        source_2 = int(handling[1]) if handling[1].isnumeric(
-        ) else wires[handling[1]]
-        if handling[2] == 'AND':
-            wires[dest] = (source_1 & source_2) & 0xFFFF
-            continue
-        if handling[2] == 'LSHIFT':
-            wires[dest] = (source_1 << source_2) & 0xFFFF
-            continue
-        if handling[2] == 'RSHIFT':
-            wires[dest] = (source_1 >> source_2) & 0xFFFF
-            continue
-        if handling[2] == 'OR':
-            wires[dest] = (source_1 | source_2) & 0xFFFF
-            continue
-
-        raise Exception()
-
-    wire_options = list(wires)
-    wire_options.sort()
-
-    return wires[wire_options[0]]
+    return process_wires(wires, gates)
 
 
 def p2(input_string: str) -> str:
+    (wires, gates) = setup_wires_and_gates(input_string)
+
+    wires['b'] = int(p1(input_string))
+
+    return process_wires(wires, gates)
+
+
+def setup_wires_and_gates(input_string):
     # letter : value
     wires = {}
     # output : (s1, s2, op)
@@ -101,6 +30,7 @@ def p2(input_string: str) -> str:
             return
         gate_instr = re.search(
             r'^(?P<s1>[a-z0-9]+)?\s?(?P<gate>AND|LSHIFT|RSHIFT|NOT|OR)\s(?P<s2>[a-z0-9]+)\s->\s(?P<dest>[a-z]+)$', instruction)
+
         if gate_instr:
             gate = gate_instr.group('gate')
             dest = gate_instr.group('dest')
@@ -126,13 +56,21 @@ def p2(input_string: str) -> str:
 
     solutions.y2015.lib2015.process_by_line(input_string, func)
 
-    wires['b'] = int(p1(input_string))
+    return (wires, gates)
 
-    queue = list(gates)
 
-    while len(queue) > 0:
-        dest = queue.pop(0)
+def process_wires(wires, gates):
+    gates_to_process = list(gates)
+
+    def have_gates_to_process()->bool:
+        return len(gates_to_process) > 0
+
+    while have_gates_to_process():
+
+        dest = gates_to_process.pop(0)
+        
         handling = gates[dest]
+        
         has_sources = True
         if handling[0] not in wires and not handling[0].isnumeric():
             has_sources = False
@@ -141,7 +79,7 @@ def p2(input_string: str) -> str:
                 has_sources = False
 
         if not has_sources:
-            queue.append(dest)
+            gates_to_process.append(dest)
             continue
 
         source_1 = int(handling[0]) if handling[0].isnumeric(
