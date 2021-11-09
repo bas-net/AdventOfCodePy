1042021312c6590f8f7eac8c63ab554f4bb2e270
bas-net <88321527+bas-net@users.noreply.github.com>
2021-11-09 08:09:28 +0100

    2015.14.2

--- src/solutions/y2015/d14.py
+++ src/solutions/y2015/d14.py
@@ -4,20 +4,55 @@ import solutions.y2015.lib2015
 
 
 def p1(input_string: str) -> str:
+    reindeers = get_reindeers(input_string)
+
+    return get_distance_of_fastest_at_time(reindeers, 1000 if len(reindeers) == 2 else 2503)
+
+
+def p2(input_string: str) -> str:
+
+    reindeers = get_reindeers(input_string)
+
+    # Name: (distance, flying_for_resting_for, state)
+    distances = {}
+    for reindeer in reindeers:
+        distances[reindeer[0]] = (0, 0, 'FLYING')
+    
+    points = {}
+    for reindeer in reindeers:
+        points[reindeer[0]] = 0
+
+    for i in range(1000 if len(reindeers) == 2 else 2503):
+        for reindeer in reindeers:
+            current = distances[reindeer[0]]
+            if current[2] == 'FLYING' and current[1] < reindeer[2]:
+                distances[reindeer[0]] = (current[0] + reindeer[1], current[1] + 1, 'FLYING')
+            elif current[2] == 'FLYING' and current[1] == reindeer[2]:
+                distances[reindeer[0]] = (current[0], 1, 'RESTING')
+            elif current[2] == 'RESTING' and current[1] < reindeer[3]:
+                distances[reindeer[0]] = (current[0], current[1] + 1, 'RESTING')
+            elif current[2] == 'RESTING' and current[1] == reindeer[3]:
+                distances[reindeer[0]] = (current[0] + reindeer[1], 1, 'FLYING')
+            else:
+                raise Exception()
+        
+        max_dist = max([x[0] for x in distances.values()])
+        for reindeer_name, information in distances.items():
+            if information[0] == max_dist:
+                points[reindeer_name] += 1
+
+    return max(points.values())
+
+
+def get_reindeers(input_string: str) -> str:
     def func(s):
         m = re.search(
             r'(?P<name>\w+) can fly (?P<speed>\d+) km/s for (?P<flight_seconds>\d+) seconds, but then must rest for (?P<rest_seconds>\d+) seconds\.', s)
         g = m.groupdict()
         return (g['name'], int(g['speed']), int(g['flight_seconds']), int(g['rest_seconds']))
 
-    reindeers = solutions.y2015.lib2015.process_by_line_aggregate(
+    return solutions.y2015.lib2015.process_by_line_aggregate(
         input_string, func, list)
-
-    return get_distance_of_fastest_at_time(reindeers, 11 if len(reindeers) == 2 else 2503)
-
-
-def p2(input_string: str) -> str:
-    pass
 
 
 def get_distance_of_fastest_at_time(reindeers, time):
