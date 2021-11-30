3d5df27b650703cf92dbe06b39f2798c8aa22cce
bas-net <88321527+bas-net@users.noreply.github.com>
2021-11-30 22:43:39 +0100

    2015.15.2

--- src/solutions/y2015/d15.py
+++ src/solutions/y2015/d15.py
@@ -18,7 +18,8 @@ def p1(input_string: str) -> str:
         in itertools.product(range(100 + 1), repeat=len(ingredients))
         if sum(x) == 100
     ]:
-        result = apply_function(ingredients, ingredient_counts)
+        result = apply_function(get_total_attributes(
+            ingredients, ingredient_counts))
         if max_result:
             max_result = max(result, max_result)
         else:
@@ -28,7 +29,32 @@ def p1(input_string: str) -> str:
 
 
 def p2(input_string: str) -> str:
-    pass
+    ingredients = [get_ingredient(line) for line in input_string.split('\n')]
+    # x,y,z,a
+
+    max_result = None
+
+    for ingredient_counts in [
+        list(x)
+        for x
+        in itertools.product(range(100 + 1), repeat=len(ingredients))
+        if sum(x) == 100
+    ]:
+        total_attributes = get_total_attributes(
+            ingredients, ingredient_counts, with_calories=True)
+
+        if total_attributes['calories'] != 500:
+            continue
+
+        result = apply_function(get_total_attributes(
+            ingredients, ingredient_counts))
+
+        if max_result:
+            max_result = max(result, max_result)
+        else:
+            max_result = result
+
+    return max_result
 
 
 def get_ingredient(input_string: str) -> Dict:
@@ -45,21 +71,22 @@ def get_ingredient(input_string: str) -> Dict:
     }
 
 
-def apply_function(ingredients: List[Dict], multipliers: List[int]) -> int:
-    result = 1
+def get_total_attributes(ingredients: List[Dict], multipliers: List[int], with_calories: bool = False) -> Dict:
     attribute_results = defaultdict(lambda: 0)
     for ingredient, multiplier in zip(ingredients, multipliers):
         attribute_results['capacity'] += ingredient['capacity'] * multiplier
         attribute_results['durability'] += ingredient['durability'] * multiplier
         attribute_results['flavor'] += ingredient['flavor'] * multiplier
         attribute_results['texture'] += ingredient['texture'] * multiplier
+        if with_calories:
+            attribute_results['calories'] += ingredient['calories'] * multiplier
+
+    return dict(attribute_results)
+
+
+def apply_function(attribute_results: Dict) -> int:
+    result = 1
     for v in attribute_results.values():
         result *= max(v, 0)
-
-    # print("applying function")
-    # print(ingredients)
-    # print(multipliers)
-    # print(attribute_results)
-    # print(result)
 
     return result
