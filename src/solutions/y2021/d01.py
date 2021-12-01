28112596382025f178db24d8680b58ca5b3ffd4b
bas-net <88321527+bas-net@users.noreply.github.com>
2021-12-01 06:10:39 +0100

    2021.01.2

--- src/solutions/y2021/d01.py
+++ src/solutions/y2021/d01.py
@@ -15,4 +15,15 @@ def p1(input_string: str) -> str:
 
 
 def p2(input_string: str) -> str:
-    pass
+    p = None
+    c = 0
+    data = list(map(int, input_string.split('\n')))
+    for i in range(len(data) - 2):
+        x = data[i] + data[i+1] + data[i+2]
+        if not p:
+            p = x
+        else:
+            if p < x:
+                c += 1
+            p = x
+    return c
