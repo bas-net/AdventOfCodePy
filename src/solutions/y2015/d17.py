6841b9158dfeff165d69beccb9538b4b3e2bf31a
bas-net <88321527+bas-net@users.noreply.github.com>
2021-12-04 21:43:38 +0100

    2015.17.1

---
+++ src/solutions/y2015/d17.py
@@ -0,0 +1,76 @@
+
+from os import path, sep
+from typing import List, Tuple
+import solutions.y2015.lib2015
+
+from solutions.sharedlib import input_strings, get_dict_from_string, input_dict, input_ints
+
+from itertools import permutations
+
+
+@input_ints
+def p1(input_data) -> str:
+    if len(input_data) == 5:
+        amount_to_store = 25
+    else:
+        amount_to_store = 150
+
+    # working_options = set()
+    # for option in permutations(input_data):
+    #     total_option = 0
+    #     for i, bucket in enumerate(option):
+    #         total_option += bucket
+    #         if total_option > amount_to_store:
+    #             break
+    #         if total_option == amount_to_store:
+    #             opts = list(option[:i+1])
+    #             opts.sort()
+    #             working_options.add(tuple(opts))
+    #             break
+
+    options = list(enumerate(input_data))
+    # print(options)
+
+    options_found = recursive_find_options(amount_to_store, options)
+    # print(*options_found, sep='\n')
+    #exit()
+    return len(options_found)
+
+
+def p2(input_data) -> str:
+    pass
+
+
+def recursive_find_options(
+    amount_to_fill: int,
+    options_left: List[Tuple[int, int]],
+    total_so_far=0,
+    path_so_far=()
+) -> List[Tuple]:
+    
+    if total_so_far > amount_to_fill:
+        # print(f'r {total_so_far} {path_so_far} options_left = {options_left} => bad!')
+        return []
+    if total_so_far == amount_to_fill:
+        # print(f'r {total_so_far} {path_so_far} options_left = {options_left} => good!')
+        l = list(path_so_far)
+        l.sort()
+        return [tuple(l)]
+
+    # print(f'r {total_so_far} {path_so_far} options_left = {options_left}')
+
+    results = []
+    for i, option in enumerate(options_left):
+        # Skip the path if the option we're looking at is bigger than the last.
+        if len(path_so_far) > 0 and option[1] > path_so_far[-1][1]:
+            continue
+
+        results += recursive_find_options(amount_to_fill,
+                                          options_left[:i] +
+                                          options_left[i+1:],
+                                          total_so_far + option[1],
+                                          (*path_so_far, option))
+
+    results = list(set(results))
+
+    return results
