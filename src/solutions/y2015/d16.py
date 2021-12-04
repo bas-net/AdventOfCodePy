4f4c7349679faacf6ca63a59b685b4d2257018ff
bas-net <88321527+bas-net@users.noreply.github.com>
2021-12-04 19:12:54 +0100

    2015.16.2

--- src/solutions/y2015/d16.py
+++ src/solutions/y2015/d16.py
@@ -36,5 +36,28 @@ def p1(input_data) -> str:
     raise Exception('No matching Sue was found!')
 
 
+@input_dict(r'Sue (\d+): (\w+): (\d+), (\w+): (\d+), (\w+): (\d+)', [
+    ('sue_number', int),
+    ('p0_name', str),
+    ('p0_value', int),
+    ('p1_name', str),
+    ('p1_value', int),
+    ('p2_name', str),
+    ('p2_value', int),
+])
 def p2(input_data) -> str:
-    pass
+    sue_identifiers = get_list_of_things_about_sue()
+    for sue in input_data:
+        if all(check_identifier(sue[f'p{i}_name'], sue_identifiers[sue[f'p{i}_name']], sue[f'p{i}_value']) for i in range(3)):
+            return sue['sue_number']
+    raise Exception('No matching Sue was found!')
+
+
+def check_identifier(identifier, identifier_value, sue_value):
+    if identifier in ['cats', 'trees']:
+        return identifier_value < sue_value
+
+    if identifier in ['pomeranians', 'goldfish']:
+        return identifier_value > sue_value
+
+    return identifier_value == sue_value
