1ccadeba404362142ed9a67227b8fd2ba579ed10
bas-net <88321527+bas-net@users.noreply.github.com>
2021-12-06 21:38:59 +0100

    2021.06.1

--- src/solutions/sharedlib.py
+++ src/solutions/sharedlib.py
@@ -18,6 +18,22 @@ def input_ints(func: Callable[[List[int]], str]) -> Callable[[str], str]:
     return inner
 
 
+
+def input_ints_csv(func: Callable[[List[int]], str]) -> Callable[[str], str]:
+    def inner(*args, **kwargs) -> Any:
+        if kwargs:
+            print('Shouldn\'t have kwargs')
+        if len(args) != 1:
+            print('Should only have 1 arg')
+        if not isinstance(args[0], str):
+            print('Argument should be str')
+
+        input_string = args[0]
+
+        return func(list(map(int, input_string.split(','))))
+    return inner
+
+
 def get_dict_from_string(
     regex: Union[str, Pattern],
     properties: List[Tuple[str, Callable]],
---
+++ src/solutions/y2021/d06.py
@@ -0,0 +1,28 @@
+
+import solutions.y2021.lib2021
+
+from solutions.sharedlib import input_ints_csv, input_strings, get_dict_from_string, input_dict
+
+from collections import defaultdict
+
+
+@input_ints_csv
+def p1(input_data) -> str:
+    fishies = defaultdict(lambda: 0)
+    for i in input_data:
+        fishies[i] += 1
+
+    for i in range(80):
+        # print(fishies, sum(fishies.values()))
+        f2 = defaultdict(lambda: 0)
+        for j in range(1, 8 + 1):
+            f2[j-1] = fishies[j]
+        f2[6] += fishies[0]
+        f2[8] += fishies[0]
+        fishies = f2
+
+    return sum(fishies.values())
+
+
+def p2(input_data) -> str:
+    pass
