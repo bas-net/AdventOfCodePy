8230810323deca7d55eb746899ad48dc9b66900a
bas-net <88321527+bas-net@users.noreply.github.com>
2021-12-06 21:45:19 +0100

    2021.06.2

--- src/solutions/y2021/d06.py
+++ src/solutions/y2021/d06.py
@@ -24,5 +24,19 @@ def p1(input_data) -> str:
     return sum(fishies.values())
 
 
+@input_ints_csv
 def p2(input_data) -> str:
-    pass
+    fishies = defaultdict(lambda: 0)
+    for i in input_data:
+        fishies[i] += 1
+
+    for i in range(256):
+        # print(fishies, sum(fishies.values()))
+        f2 = defaultdict(lambda: 0)
+        for j in range(1, 8 + 1):
+            f2[j-1] = fishies[j]
+        f2[6] += fishies[0]
+        f2[8] += fishies[0]
+        fishies = f2
+
+    return sum(fishies.values())
