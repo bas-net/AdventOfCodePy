534cb9b9ad94fb7ca6f1b41c5ddf1519d222b1c4
bas-net <88321527+bas-net@users.noreply.github.com>
2021-12-07 21:10:31 +0100

    2021.7.1

---
+++ src/solutions/y2021/d07.py
@@ -0,0 +1,28 @@
+
+import solutions.y2021.lib2021
+
+from solutions.sharedlib import input_strings, get_dict_from_string, input_dict, input_ints_csv
+
+
+@input_ints_csv
+def p1(input_data) -> str:
+    lower = min(input_data)
+    higher = max(input_data)
+
+    fuel_consumption = None
+    for i in range(lower, higher + 1):
+        # print(f'trying {i}')
+        result = sum(map(lambda horizontal_position: abs(
+            i - horizontal_position), input_data))
+        # print(f'result: {result}')
+
+        if fuel_consumption == None:
+            fuel_consumption = result
+        else:
+            fuel_consumption = min(fuel_consumption, result)
+
+    return fuel_consumption
+
+
+def p2(input_data) -> str:
+    pass
