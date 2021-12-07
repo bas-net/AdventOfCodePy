7bf2078fd870f4cb90eb987a99b7d3d5b8f56af1
bas-net <88321527+bas-net@users.noreply.github.com>
2021-12-08 00:28:25 +0100

    refactor

--- src/solutions/y2021/d07.py
+++ src/solutions/y2021/d07.py
@@ -1,39 +1,28 @@
 
 import solutions.y2021.lib2021
 
-from solutions.sharedlib import input_strings, get_dict_from_string, input_dict, input_ints_csv
+from solutions.sharedlib import input_ints_csv
 
 
 @input_ints_csv
 def p1(input_data) -> str:
-    lower = min(input_data)
-    higher = max(input_data)
+    return allign_crabs(input_data,
+                        lambda i, horizontal_position: abs(i - horizontal_position))
 
-    fuel_consumption = None
-    for i in range(lower, higher + 1):
-        # print(f'trying {i}')
-        result = sum(map(lambda horizontal_position: abs(
-            i - horizontal_position), input_data))
-        # print(f'result: {result}')
-
-        if fuel_consumption == None:
-            fuel_consumption = result
-        else:
-            fuel_consumption = min(fuel_consumption, result)
-
-    return fuel_consumption
 
 @input_ints_csv
 def p2(input_data) -> str:
+    return allign_crabs(input_data,
+                        lambda i, horizontal_position: sum_all_numbers_below(abs(i - horizontal_position)))
+
+
+def allign_crabs(input_data, function):
     lower = min(input_data)
     higher = max(input_data)
 
     fuel_consumption = None
     for i in range(lower, higher + 1):
-        # print(f'trying {i}')
-        result = sum(map(lambda horizontal_position: sum_all_numbers_below(abs(
-            i - horizontal_position)), input_data))
-        # print(f'result: {result}')
+        result = sum(map(lambda x: function(i, x), input_data))
 
         if fuel_consumption == None:
             fuel_consumption = result
@@ -45,3 +34,10 @@ def p2(input_data) -> str:
 
 def sum_all_numbers_below(i):
     return sum(range(1, i + 1))
+
+
+def none_safe(func, a, b):
+    if a == None:
+        return b
+    else:
+        return func(a, b)
