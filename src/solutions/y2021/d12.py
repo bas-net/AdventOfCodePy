d9490ac03eb13f11639f5afac23d2b87641fdd63
bas-net <88321527+bas-net@users.noreply.github.com>
2021-12-12 12:24:44 +0100

    2021.12.2

--- src/solutions/y2021/d12.py
+++ src/solutions/y2021/d12.py
@@ -59,7 +59,7 @@ def p1(input_data) -> str:
         g.add_vertex(
             Vertex((Node(connection.source), Node(connection.destination))))
 
-    print(*g.nodes, sep='\n')
+    # print(*g.nodes, sep='\n')
 
     start = g.get_node('start')
     queue = [(start.id,)]
@@ -77,11 +77,55 @@ def p1(input_data) -> str:
             else:
                 queue.append((*path, connected_node.id))
 
-    # print(queue)
-    # print(full_paths)
-    # exit()
     return len(full_paths)
 
 
+@dataclass
+class P2Path:
+    path: Tuple
+    duplicate_visited_small_cave: str
+
+    @property
+    def can_visit_small_again(self):
+        return self.path.count(self.duplicate_visited_small_cave) < 2
+
+
+@input_named_tuple(r'(\w+)-(\w+)', [
+    ('source', str),
+    ('destination', str)
+])
 def p2(input_data) -> str:
-    pass
+    g = Graph([], [])
+
+    for connection in input_data:
+        g.add_vertex(
+            Vertex((Node(connection.source), Node(connection.destination))))
+
+    # print(*g.nodes, sep='\n')
+
+    start = g.get_node('start')
+    all_small_ids = [node.id for node in g.nodes if node.is_small_cave() and not node.is_start() and not node.is_end()]
+    queue = [P2Path((start.id,), None)] + [P2Path((start.id,), small_id)
+                                           for small_id in all_small_ids]
+    full_paths = []
+
+    while len(queue) > 0:
+        path = queue.pop(0)
+        last_node = g.get_node(path.path[-1])
+        for connected_node in g.get_connected_nodes(last_node):
+            if connected_node.is_small_cave() and connected_node.id in path.path and not (connected_node.id == path.duplicate_visited_small_cave and path.can_visit_small_again):
+                continue
+
+            if connected_node.is_end():
+                full_paths.append(
+                    P2Path((*path.path, connected_node.id), path.duplicate_visited_small_cave))
+            else:
+                queue.append(P2Path((*path.path, connected_node.id),
+                             path.duplicate_visited_small_cave))
+
+    # print(*full_paths, sep='\n')
+    # exit()
+
+    full_paths = set(p2path.path for p2path in full_paths)
+
+    return len(full_paths)
